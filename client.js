addf8fe7744aae530b86743620ea8817709b2d77
HerbCaudill <herb@devresults.com>
2020-12-09 21:16:25 +0100

    first commit

---
+++ client.js
@@ -0,0 +1,29 @@
+﻿const net = require('net')
+const HOST = '192.168.7.233'
+const PORT = 9876
+const prompt = require('prompt-sync')()
+
+const socket = new net.Socket()
+
+// Connecting to a TCP port
+socket.connect(PORT, HOST, () => {
+  console.log(`[${HOST}:${PORT}] connected`)
+
+  while (true) {
+    const message = prompt('>')
+    socket.write(message)
+  }
+})
+
+// Receiving data from the server
+socket.on('data', data => {
+  connection.receiveData(data)
+})
+
+socket.on('close', () => {
+  console.log(`[${HOST}:${PORT}] connection closed`)
+})
+
+socket.on('error', err => {
+  console.log(`[${socket.remoteAddress}:${socket.remotePort}] error: ${err}`)
+})
---
+++ server.js
@@ -0,0 +1,33 @@
+﻿const net = require('net')
+const HOST = '192.168.7.233'
+const PORT = 9876
+
+const messages = []
+
+const update = () => {
+  console.clear()
+  messages.forEach(m => console.log(m))
+}
+
+function handler(socket) {
+  console.log(`[${socket.remoteAddress}:${socket.remotePort}] connected`)
+
+  // Receiving data from the client
+  socket.on('data', data => {
+    messages.push(data.toString())
+    update()
+  })
+
+  socket.on('close', data => {
+    console.log(`connection closed`)
+    connection.close()
+  })
+
+  socket.on('error', err => {
+    console.log(`[${socket.remoteAddress}:${socket.remotePort}] error: ${err}`)
+  })
+}
+
+// Listen on a TCP port
+net.createServer(handler).listen(PORT, HOST)
+console.log(`Listening on ${HOST}:${PORT}`)
